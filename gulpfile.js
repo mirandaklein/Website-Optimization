26aaae3dc9608a263455273c135572e0dc613df9
mirandaklein <mirandaklein@mac.com>
2017-08-29 14:16:40 -0600

    JS Minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
     imagemin = require ('gulp-imagemin');
+    minify = require('gulp-minify');
    
 // image compression
 
@@ -8,4 +9,24 @@ gulp.task('image', function() {
         .pipe(imagemin())
         .pipe(gulp.dest('compressed'));
 });
+
+gulp.task('image', function() {
+    gulp.src('views/image/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('compressed'));
+});
+
+// JS compression
+gulp.task('compress', function() {
+    gulp.src('views/js/*')
+      .pipe(minify({
+          ext:{
+              src:'-debug.js',
+              min:'.js'
+          },
+          exclude: ['tasks'],
+          ignoreFiles: ['.combo.js', '-min.js']
+      }))
+      .pipe(gulp.dest('dist'))
+  });
 
